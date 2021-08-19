d00da441c46ab2f55a2c91197285103bd1ea425f
Mert <mertuygur02@gmail.com>
2021-08-19 23:38:03 +0300

    sidebar  completed, mds are showing up as expected. Looking nice but not responsive yet

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,26 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState } from "react";
 import Footer from "./Footer";
 import Link from "next/link";
 import classes from "./layout.module.css";
 
 const Layout = ({ children }) => {
+  const [toggleMenu, setToggleMenu] = useState(true);
+  const [toggleMenu2, setToggleMenu2] = useState(false);
+  const [toggleMenu3, setToggleMenu3] = useState(false);
+
+  const toggleHandler = (e) => {
+    e.preventDefault();
+    setToggleMenu(!toggleMenu);
+  };
+  const toggleHandler2 = (e) => {
+    e.preventDefault();
+    setToggleMenu2(!toggleMenu2);
+  };
+  const toggleHandler3 = (e) => {
+    e.preventDefault();
+    setToggleMenu3(!toggleMenu3);
+  };
+
   return (
     <div>
       <main className={classes.main}>
@@ -15,29 +31,79 @@ const Layout = ({ children }) => {
 
           <div className={classes.routes}>
             <div className={classes.group}>
-              <h3 className={classes.grouptitle}>Getting Started</h3>
-              <ul className={classes.grouplist}>
-                <li>
-                  <Link href="/docs/jest">Inside Item0</Link>
-                </li>
-                <li>
-                  <Link href="/docs/test">Inside Item1</Link>
-                </li>
-                <li>
-                  <Link href="/docs/mest">Inside Item2</Link>
-                </li>
-                <li>
-                  <Link href="/">Inside Item3</Link>
-                </li>
-                <li>
-                  <Link href="/">Inside Item4</Link>
-                </li>
-              </ul>
+              <h3 className={classes.grouptitle} onClick={toggleHandler}>
+                Getting Started
+              </h3>
+              {toggleMenu && (
+                <ul className={classes.grouplist}>
+                  <li>
+                    <Link href="/docs/getting-started">Inside Item0</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/test">Inside Item1</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/mest">Inside Item2</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/python">Inside Item3</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/test">Inside Item4</Link>
+                  </li>
+                </ul>
+              )}
             </div>
-          </div>
 
-          <div>
-            <Link href="/">First Project</Link>
+            <div className={classes.group}>
+              <h3 className={classes.grouptitle} onClick={toggleHandler2}>
+                Getting Started
+              </h3>
+              {toggleMenu2 && (
+                <ul className={classes.grouplist}>
+                  <li>
+                    <Link href="/docs/jest">Inside Item0</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/test">Inside Item1</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/mest">Inside Item2</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/python">Inside Item3</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/test">Inside Item4</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+
+            <div className={classes.group}>
+              <h3 className={classes.grouptitle} onClick={toggleHandler3}>
+                Getting Started
+              </h3>
+              {toggleMenu3 && (
+                <ul className={classes.grouplist}>
+                  <li>
+                    <Link href="/docs/jest">Inside Item0</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/test">Inside Item1</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/mest">Inside Item2</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/python">Inside Item3</Link>
+                  </li>
+                  <li>
+                    <Link href="/docs/test">Inside Item4</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
           </div>
         </section>
         <section className={classes.content}>{children}</section>
--- pages/docs/[slug].js
+++ pages/docs/[slug].js
@@ -1,51 +1,58 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import marked from 'marked'
-import './doc.module.css';
+import React from "react";
+import Layout from "../../components/Layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+import "./doc.module.css";
+import classes from "./doc.module.css";
 
-const Doc = ({frontmatter: {title, date, excerpt}, slug, content}) => {
+const Doc = ({ frontmatter: { title, date, excerpt }, slug, content }) => {
   return (
     <Layout>
-      <div>
-        <h2>{title}</h2>
-        <p>{date}</p>
-        <hr />
-        <div>
-          <div dangerouslySetInnerHTML={{__html: marked(content)}}></div>
+      <div className={classes.wrapper}>
+        <div className={classes.header}>
+          <h2 className={classes.title}>{title}</h2>
+          <p className={classes.excerpt}>{excerpt}</p>
+          <p className={classes.date}>{date}</p>
+        </div>
+
+        <div className={classes.content}>
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('docs'));
+  const files = fs.readdirSync(path.join("docs"));
   const paths = files.map((filename) => ({
     params: {
-      slug: filename.replace('.md', '')
-    }
-  }))
+      slug: filename.replace(".md", ""),
+    },
+  }));
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({params: {slug}}){
-  const markdownWithMeta = fs.readFileSync(path.join('docs', slug + '.md'), 'utf-8') 
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(
+    path.join("docs", slug + ".md"),
+    "utf-8"
+  );
 
-  const {data:frontmatter, content} = matter(markdownWithMeta)
+  const { data: frontmatter, content } = matter(markdownWithMeta);
   return {
     props: {
       frontmatter,
       slug,
-      content
-    }
-  }
+      content,
+    },
+  };
 }
 
-export default Doc
+export default Doc;
