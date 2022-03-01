bf858ed4302a4abb1902fe992d7a34956425702e
mert18 <mertuygur02@gmail.com>
2022-03-01 22:08:51 +0300

    font size

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,42 @@
-import Head from 'next/head'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import Layout from '../components/Layout'
-
+import Head from "next/head";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Do Docc</title>
+        <title>Do Doc</title>
       </Head>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   // Get files from the docs dir.
-  const files = fs.readdirSync(path.join('docs'))
+  const files = fs.readdirSync(path.join("docs"));
   // Get slug and frontmatter
-  const docs = files.map(filename => {
+  const docs = files.map((filename) => {
     // Create slug
-    const slug = filename.replace('.md', '')
+    const slug = filename.replace(".md", "");
 
     // Get frontmatter
-    const markdownWithMeta = fs.readFileSync(path.join('docs', filename), 'utf-8')
+    const markdownWithMeta = fs.readFileSync(
+      path.join("docs", filename),
+      "utf-8"
+    );
 
-    const {data:frontmatter} =matter(markdownWithMeta)
-    return{
+    const { data: frontmatter } = matter(markdownWithMeta);
+    return {
       slug,
-      frontmatter
-    }
-  })
-  return{
-    props:{
-      docs: docs
-    }
-  }
+      frontmatter,
+    };
+  });
+  return {
+    props: {
+      docs: docs,
+    },
+  };
 }
-
